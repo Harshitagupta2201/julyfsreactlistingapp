5b7dd54f315a12a8d0916907ff7e8f1c233f8a24
shubh <shubhamdixit863@gmail.com>
2022-11-07 19:35:35 +0530

    use effect

---
+++ src/route.js
@@ -0,0 +1,21 @@
+import App from "./App";
+import Listing from "./pages/Listing";
+
+
+const Routes=[
+    {
+        path: "/",
+    element: <App />,
+    children:[
+        {
+            path: "listing",
+            element: <Listing/>,
+        }
+
+    ]
+
+    },
+    ]
+
+
+    export default Routes;
